4cfd64ee6af15fb4da6a65cb8de25ca2a59a364a
code8086 <code8086@qq.com>
2015-07-29 22:43:51 +0800

    first commit

---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import socket
+host="localhost"
+port=10000
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(5)
+while 1:
+ sock,addr=s.accept()
+ print "got connection form ",sock.getpeername()
+ data=sock.recv(1024)
+ if not data:
+  break
+ else:
+  print data
+
+
+
